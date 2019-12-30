83b38c5344d3122b8654de693c19877a5cbfebf2
Jonas Hamerski <jonashamerski87@gmail.com>
2019-12-29 21:51:31 -0300

    Concluído tipo de dado None

---
+++ resumos/tipo_none.py
@@ -0,0 +1,15 @@
+"""
+NONE
+
+O tipo de dado None, representa o tipo sem tipo, ou poderia ser conhecido também como tipo vazio
+OBS: O tipo de dado None, é sempre representado pela primeira letra maíuscula
+Correto: None
+Errado: none
+
+Quando utilizamos?
+    - Podemos utilizar None quando queremos criar uma variável e inicializa-la com tipo sem tipo, antes de recebermos
+um valor
+"""
+number = None
+
+print(number)
